95ccec894e89baa9b34d3a976c9ca669fd950a99
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2020-02-18 16:36:57 +0100

    Documentation, basic lint

--- glicko2.go
+++ glicko2.go
@@ -1,3 +1,7 @@
+// Package glocko2 implements Glicko2 calculations in Go
+//
+// This package implements the Glicko2 chess rating system in Go. It can be
+// used for any 1v1 game in order to rank players on a strength scale.
 package glocko2
 
 import (
@@ -9,18 +13,28 @@ const (
 	ε       = 0.000001
 )
 
+// Player represents a player in the ranking
 type Player struct {
-	Id        string  // Player Identification
-	Name      string  // Player name
-	R         float64 // Player ranking
-	Rd        float64 // Ranking deviation
-	Sigma     float64 // Volatility
-	Active	bool // Is the player currently active?
+	// Identification of the player. Unique.
+	Id string
+	// Player name
+	Name string
+	// Current rank, R, of the player
+	R float64
+	// Ranking deviation, the systems confidence in R
+	Rd float64
+	// Volatility of the ranking. How surprising it is the ranking system.
+	Sigma float64
+	// True if the player is currently actively playing games
+	Active bool
 }
 
+// Opponent represents an opponent for the player
 type Opponent struct {
-	Idx	int	// Player index
-	Sj float64 // Match score
+	// Player index into an []Player array
+	Idx int
+	// Match score
+	Sj float64
 }
 
 type opp struct {
@@ -31,6 +45,7 @@ type opp struct {
 	sj    float64
 }
 
+// Scale transforms the external rating values into the internal rating values
 func Scale(r float64, rd float64) (mu float64, phi float64) {
 	mu = (r - 1500.0) / scaling
 	phi = (rd / scaling)
@@ -98,6 +113,10 @@ func sign(x float64) float64 {
 	}
 }
 
+// Computing volatility requires us to find a root of a function. We
+// use a numerically stable root finding method here. In the Glicko2
+// systems, there have been some variants of this historically, because
+// some root finding methods aren't numerically stable.
 func computeVolatility(sigma float64, phi float64, v float64, delta float64, tau float64) float64 {
 	a := math.Log(sigma * sigma)
 	phi2 := phi * phi
@@ -111,7 +130,7 @@ func computeVolatility(sigma float64, phi float64, v float64, delta float64, tau
 	}
 
 	var b float64
-	if delta*delta > phi*phi + v {
+	if delta*delta > phi*phi+v {
 		b = math.Log(delta*delta - phi*phi - v)
 	} else {
 		b = volK(f, a, tau)
@@ -121,33 +140,34 @@ func computeVolatility(sigma float64, phi float64, v float64, delta float64, tau
 	fb := f(b)
 
 	var c, fc, d, fd float64
-	for i := 100; i > 0 ; i-- {
+	for i := 100; i > 0; i-- {
 		if math.Abs(b-a) <= ε {
 			return math.Exp(a / 2)
-		} else {
-			c = (a + b) * 0.5
-			fc = f(c)
-			d = c + (c-a)*(sign(fa-fb)*fc)/math.Sqrt(fc*fc-fa*fb)
-			fd = f(d)
+		}
 
-			if sign(fd) != sign(fc) {
-				a = c
-				b = d
-				fa = fc
-				fb = fd
-			} else if sign(fd) != sign(fa) {
-				b = d
-				fb = fd
-			} else {
-				a = d
-				fa = fd
-			}
+		c = (a + b) * 0.5
+		fc = f(c)
+		d = c + (c-a)*(sign(fa-fb)*fc)/math.Sqrt(fc*fc-fa*fb)
+		fd = f(d)
+
+		if sign(fd) != sign(fc) {
+			a = c
+			b = d
+			fa = fc
+			fb = fd
+		} else if sign(fd) != sign(fa) {
+			b = d
+			fb = fd
+		} else {
+			a = d
+			fa = fd
 		}
 	}
 
 	panic("Exceeded iterations")
 }
 
+// PhiStar computes rating deviations for inactive players
 func PhiStar(sigmap float64, phi float64) float64 {
 	return math.Sqrt(phi*phi + sigmap*sigmap)
 }
@@ -163,14 +183,18 @@ func newRating(phis float64, mu float64, v float64, sopp []opp) (float64, float6
 	return mup, phip
 }
 
+// Unscale reverts the transformation done by the Scale function
 func Unscale(mup float64, phip float64) (float64, float64) {
 	rp := scaling*mup + 1500.0
 	rdp := scaling * phip
 	return rp, rdp
 }
 
+// Rank computes the new rank of player p
+//
+// Requires a database of players and a list of matches in opponents
 func (p *Player) Rank(opponents []Opponent, players []Player, tau float64) (float64, float64, float64) {
-	
+
 	mu, phi := Scale(p.R, p.Rd)
 	sopps := scaleOpponents(mu, opponents, players)
 	v := updateRating(sopps)
